6cb0a12c84fe62460396b8b632f4b839b03d088c
Tom Ibarreche <ibarreche.tom@outlook.com>
2024-10-28 13:49:39 +0100

    Before removing useless async stuff

---
+++ src/main.py
@@ -0,0 +1,35 @@
+from fastapi import FastAPI
+from .config import settings
+from contextlib import asynccontextmanager
+from src.db import init_db
+from .routers import registered_routers
+from .errors import register_errors
+
+@asynccontextmanager
+async def life_span(app:FastAPI):
+    print("start")
+    await init_db()
+    yield
+
+    print("after")
+
+app = FastAPI(
+    lifespan=life_span,
+    version=settings.api_version,
+    title=settings.api_name,
+    summary=settings.api_summary,
+    contact={
+        "name":settings.developer_name,
+        "url": settings.developer_profile, 
+        "email": settings.developer_email
+    }
+)
+
+registered_routers(app)
+register_errors(app)
+
+
+
+
+
+
